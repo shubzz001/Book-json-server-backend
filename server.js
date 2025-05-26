1289e824a4147bb0cb475902b8fd478e243b1235
Shubzz001 <shubhamzende1311@gmail.com>
2025-05-26 21:32:58 +0530

    Update book data in db.json and comment out server.js code

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const jsonServer = require('json-server');
-const path = require('path');
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'src/server/db.json'));
-const middlewares = jsonServer.defaults();
+// const jsonServer = require('json-server');
+// const path = require('path');
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, 'src/server/db.json'));
+// const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// server.use(middlewares);
+// server.use(router);
+// const PORT = process.env.PORT || 10000;
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });
